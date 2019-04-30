40aa40219a23b4c8169f7ff3915f3ecd5a7b2200
Hamlet Petrosyan <Hamlet Petrosyan@USER-022>
2019-04-30 20:14:40 +0400

    edit dropdown

--- react-client/src/components/Reusable/dropdown.jsx
+++ react-client/src/components/Reusable/dropdown.jsx
@@ -2,36 +2,35 @@ import React, { Component } from "react";
 import "../../assets/scss/select.scss";
 import SelectFX from "periodicjs.component.selectfx";
 class DropDown extends Component {
-  componentWillUpdate() {
-
-  }
+  componentWillUpdate() {}
   render() {
     return (
-      <section>
-        <select
-          ref="select"
-          value={this.props.selectedValue}
-          disabled={this.props.disabled}
-          onChange={this.props.onChange}
-          id={this.props.id}
-          className="cs-select cs-skin-elastic"
-        >
-          <option value="" disabled selected>
-            {this.props.label}
-          </option>
-
-          <option value="" />
-          {this.props.options.map(option => (
-            <option
-              disabled={option.disabled}
-              key={option.value}
-              value={option.value}
-            >
-              {option.name}
-            </option>
-          ))}
-        </select>
-      </section>
+      <div
+        className={"select-group" + (this.props.disabled ? " disabled" : "")}
+      >
+        <div className="select-wrapper">
+          <div className="select-label"> {this.props.label}</div>
+          <select
+            ref="select"
+            value={this.props.selectedValue}
+            disabled={this.props.disabled}
+            onChange={this.props.onChange}
+            id={this.props.id}
+          >
+            <option value="" />
+            {this.props.options.map(option => (
+              <option
+                disabled={option.disabled}
+                key={option.value}
+                value={option.value}
+              >
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <i class="fas fa-chevron-down" />
+      </div>
 
       // <div>
       //   <label htmlFor={this.props.id}>{this.props.label}</label>
